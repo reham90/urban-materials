5630452370dcb486954cd3cf1a2088e4bdafd2f5
reham90 <rehamadel344@gmail.com>
2022-12-05 12:42:15 +0300

    edits

---
+++ js/script.js
@@ -0,0 +1,142 @@
+let hint = document.querySelector(".preloader");
+window.onload = function () {
+  //hide the preloader
+  setTimeout(function () {
+    hint.style.display = "none";
+  }, 700);
+};
+$(document).ready(function () {
+  new WOW().init();
+
+  //phone size menu onclick
+  if ($(window).width() <= 991) {
+    $("#menu-id").click(function (e) {
+      e.preventDefault();
+
+      $(".navgition").toggleClass("reset-left");
+      $("body").toggleClass("overflow");
+      $(".menu-bars").toggleClass("open-bars");
+    });
+    $(".nav-head .close-btn").click(function () {
+      $(".navgition").removeClass("reset-left");
+      $(".menu-bars .bars").toggleClass("open-bars");
+      $(".menu-bars .bars").toggleClass("close-bars");
+      $("body").removeClass("overflow");
+    });
+  }
+  $(".lang-word").click(function (e) {
+    e.preventDefault();
+    var item = $(this).siblings(".dropdown-content");
+    item.slideToggle(400);
+  });
+   //slide down menu
+   $(".btn-div").click(function (e) {
+    e.preventDefault();
+    $(this).siblings(".cats-dispaly").slideToggle(400);
+    $(".btn-div").not(this).siblings(".cats-dispaly").slideUp(400);
+    if ($(window).width() <= 991) {
+      $(this).toggleClass("active");
+      $(".btn-div").not(this).removeClass("active");
+    }
+  });
+
+  //fixed nav
+  $stickyNav = $(".top-header");
+  $(window).on("scroll load", function () {
+    var scroll = $(window).scrollTop();
+    if (scroll >= 200) {
+      $stickyNav.addClass("fixed-nav", 500);
+    } else {
+      $stickyNav.removeClass("fixed-nav", 500);
+    }
+    if (scroll == 0) {
+      $stickyNav.removeClass("fixed-nav", 500);
+    }
+  });
+  var $stickyheader = $("header");
+  lastScroll = 0;
+  $(window).on("scroll load", function () {
+    var scroll = $(window).scrollTop();
+    if (lastScroll - scroll > 0) {
+      $stickyheader.addClass("fixed-header", { duration: 1000 });
+    } else if (lastScroll - scroll >= 0 && $(window).width() <= 991) {
+      $stickyheader.addClass("fixed-header", { duration: 1000 });
+    } else {
+      $stickyheader.removeClass("fixed-header", { duration: 500 });
+    }
+    lastScroll = scroll;
+    if (scroll == 0) {
+      $stickyheader.removeClass("fixed-header", { duration: 500 });
+    }
+  });
+  ///////// ** main** /////////
+  var specials = new Swiper(".main-slider .swiper-container", {
+    loop: true,
+    autoplay: true,
+    pagination: {
+      el: ".main-slider .swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".main-slider .swiper-btn-next",
+      prevEl: ".main-slider .swiper-btn-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 1,
+      },
+      992: {
+        slidesPerView: 1,
+      },
+      1199: {
+        slidesPerView: 1,
+      },
+    },
+  });
+ 
+  ////////////** footer transfer into accordion **//////////
+
+  if ($(window).width() <= 767) {
+    $(".nav-foot-header").addClass("footer-accordion");
+    $(".nav-foot").addClass("footer-panel");
+  }
+  $(".footer-accordion").click(function () {
+    var x = $(this).siblings().prop("scrollHeight") + 15 + "px";
+    $(".footer-accordion").not(this).removeClass("active");
+    $(this).toggleClass("active");
+    if ($(this).siblings().css("max-height") == "0px") {
+      $(this).siblings().css("max-height", x);
+      $(this).siblings(".nav-foot").css("padding-top", "15px");
+    } else {
+      $(this).siblings().css("max-height", "0");
+      $(this).siblings(".nav-foot").css("padding-top", "0");
+    }
+
+    $(".footer-accordion").not(this).siblings().css("max-height", "0");
+    $(".footer-accordion")
+      .not(this)
+      .siblings(".nav-foot")
+      .css("padding-top", "0");
+  });
+  //////////** fixed arrow to top**//////////
+  $(".arrow-top").click(function () {
+    $("html,body").animate(
+      {
+        scrollTop: 0,
+      },
+      1500
+    );
+  });
+  $(this).scrollTop() >= 500
+    ? $(".arrow-top").fadeIn(300)
+    : $(".arrow-top").fadeOut(300);
+
+  $(window).scroll(function () {
+    $(this).scrollTop() >= 500
+      ? $(".arrow-top").fadeIn(300)
+      : $(".arrow-top").fadeOut(300);
+  });
+});
